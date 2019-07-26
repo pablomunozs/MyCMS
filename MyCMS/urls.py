e76e637512203c5ec7baa4b1ec283a484b72b63f
Pablo Muñoz Schmidt <pablomunozs@gmail.com>
2019-07-26 17:32:39 +0200

    Updating urls and settings

--- MyCMS/urls.py
+++ MyCMS/urls.py
@@ -13,10 +13,8 @@ from django.views.static import serve
 admin.autodiscover()
 
 urlpatterns = [
-url(r'^media/(?P<path>.*)$', serve,
-            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-#    url(r'^sitemap\.xml$', sitemap,
-#        {'sitemaps': {'cmspages': CMSSitemap}}),
+    url(r'^sitemap\.xml$', sitemap,
+        {'sitemaps': {'cmspages': CMSSitemap}}),
 ]
 
 urlpatterns += i18n_patterns(
